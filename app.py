e3beff49674569798c34a0be2285818b226b7d57
Zac <lon22came01@mpw.ac.uk>
2023-11-30 13:28:26 +0000

    Revert "Update app.py"
    This reverts commit 6a80941522b87863491fdedf83470254653b32b3.

--- app.py
+++ app.py
@@ -149,8 +149,8 @@ def search():
     global global_sterm
     p = request.args.get("search")
     r = asearch.search(p)
-    #jobid = r["job_id"]
-    jobid = "656889f415ee975ae027a6cf"
+    jobid = r["job_id"]
+    #jobid = "64f490ef45f4e74352a4aab7"
     print(jobid)
     global_sterm = p
     o = result.result (jobid)
