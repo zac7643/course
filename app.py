ca32802aedfe7396e46353052a07eb09bc1165e0
Zac <lon22came01@mpw.ac.uk>
2023-12-26 21:10:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def login():
 
         cur = con.cursor()
         cur.execute("""
-        SELECT favs.*, stats.product_price FROM favs 
+        SELECT favs.*, stats.product_price_stats FROM favs 
         INNER JOIN stats ON favs.id = stats.fav_id 
         WHERE favs.USERNAME = ?
         """, (username,))
