9c84bfae4d1c1c1366981068ec3a770e2dbbbddb
Zac <lon22came01@mpw.ac.uk>
2023-12-29 14:43:22 +0000

    Update stats.py

--- stats.py
+++ stats.py
@@ -23,28 +23,26 @@ def main():
         st.write(f"Error: {str(e)}")
         return
 
+    # Calculate highest price, lowest price, and average price
+    highest_price = chart_data['product_price_stats'].max()
+    lowest_price = chart_data['product_price_stats'].min()
+    average_price = chart_data['product_price_stats'].mean()
+
     try:
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=chart_data['price_date_stats'], y=chart_data['product_price_stats'], mode='lines+markers', 
                                  name='Price History', line=dict(color='#FF9900')))
         fig.update_layout(title='Price History', xaxis_title='Date', yaxis_title='Price', autosize=True, 
-                          template='plotly_dark', title_x=0.5, font=dict(size=18))
-        fig.update_xaxes(title_font=dict(size=30))  
-        fig.update_yaxes(title_font=dict(size=30)) 
-        fig.update_xaxes(tickfont=dict(size=16))
-        fig.update_yaxes(tickfont=dict(size=16))  
+                          template='plotly_dark', title_x=0.5, font=dict(size=18),
+                          annotations=[
+                              dict(xref='paper', yref='paper', x=0.95, y=1.05, showarrow=False,
+                                   text=f'Highest Price: {highest_price}<br>Lowest Price: {lowest_price}<br>Average Price: {average_price:.2f}')
+                          ])
+        fig.update_xaxes(tickfont=dict(size=16))  # Increase x-axis tick label size
+        fig.update_yaxes(tickfont=dict(size=16))  # Increase y-axis tick label size
         st.plotly_chart(fig, use_container_width=True)
     except Exception as e:
         st.write(f"Error creating chart: {e}")
-        
-    # Calculate and display highest price, lowest price, and average price
-    highest_price = chart_data['product_price_stats'].max()
-    lowest_price = chart_data['product_price_stats'].min()
-    average_price = chart_data['product_price_stats'].mean()
-
-    st.write(f"Highest Price: {highest_price}")
-    st.write(f"Lowest Price: {lowest_price}")
-    st.write(f"Average Price: {average_price}")
 
 if __name__ == '__main__':
     main()
